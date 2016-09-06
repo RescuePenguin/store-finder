6123d62b4d7216f6dcb187ea6f1cd00794e38ee4
Jake Holzhauer <jake.holzhauer@yahoo.com>
2016-09-06 01:45:33 -0700

    Display the information pertaining to the company after uploading
    Take all the locations added by the user and display them back to
    the user, ordered by distance.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -14,5 +14,12 @@ class CompaniesController < ApplicationController
   end
 
   def show
+    @company = Company.find params[:id]
+    search = Geocoder.search(params[:search]) if params[:search].present?
+    default_loc = Geocoder.search([33.1243208, -117.32582479999996])[0]
+    @radius = params[:radius].present? ? params[:radius] : 2000
+    @latitude = search.present? ? search.latitude : default_loc.latitude
+    @longitude = search.present? ? search.longitude : default_loc.longitude
+    @locations = @company.locations.near([@latitude, @longitude], @radius)
   end
 end
