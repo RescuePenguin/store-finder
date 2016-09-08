54cd09d6a2fa6a28da811b2e9199dc00f30efca3
Jake Holzhauer <jake.holzhauer@yahoo.com>
2016-09-08 03:09:08 -0700

    Better tables, and google map
    Cleaned up the tables for our lists
    Added a google map interactable on company#show.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -7,6 +7,10 @@ class CompaniesController < ApplicationController
 
   def create
     @company = Company.new
+    if params[:company][:upload].nil?
+      flash[:alert] = 'Please upload a .tsv file'
+      return redirect_to new_company_path
+    end
     if !['.tsv'].include? File.extname(params[:company][:upload].tempfile)
       flash[:alert] = 'Please upload a .tsv file'
       return redirect_to new_company_path
@@ -26,9 +30,9 @@ class CompaniesController < ApplicationController
     @company = Company.find params[:id]
     default_loc = Geocoder.search([33.1243208, -117.32582479999996])[0]
     search = Geocoder.search(params[:search])[0] if params[:search].present?
-    @radius = params[:radius].present? ? params[:radius] : 2000
+    @radius = params[:radius].present? ? params[:radius] : 25
     @latitude =  params[:search].present? ? search.latitude : default_loc.latitude
     @longitude = params[:search].present? ? search.longitude : default_loc.longitude
-    @locations = @company.locations.near([@latitude, @longitude], @radius)
+    @locations = @company.locations.near([@latitude, @longitude], @radius).limit(50)
   end
 end
